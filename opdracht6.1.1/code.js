1ec3226279ec38f00897059d128a229044cfb71b
mehmetku <9016885@student.zadkine.nl>
2023-10-29 21:42:21 +0100

    first commit

--- opdracht6.1.1/code.js
+++
@@ -1,5 +0,0 @@
-javascript
-function kleur_aanpassen() {
-  var h1Element = document.querySelector("h1");
-  h1Element.style.color = "red";
-}
--- opdracht6.2.2/code.js
+++
@@ -1,5 +0,0 @@
-function KleurEnTekstAanpassen()
-{
-    document.getElementById("kop1").style.color = "red";
-    document.getElementById("kop1").innerHTML = "I have changed!";
-}
